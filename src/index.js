3f8738d9862e67d2c61ce42832321f94320ce583
Eyoel W Feleke <feleke.e@northeastern.edu>
2022-05-06 14:39:54 -0500

    heroku setup

--- src/index.js
+++ src/index.js
@@ -20,18 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-// ReactDOM.render(
-//   <div>
-//     <BrowserRouter>
-//       {/* {NavBar goes here} */}
-//       {/* <NavBar /> */}
-//       <Routes>
-//         {/* <Route path={"/"} element={<App />} />
-//         <Route path={"/home/:homeId"} element={<HomeEntry />} />
-//         <Route path={"/login"} element={<Login />} />
-//         <Route path={"/signup"} element={<SignUp />} /> */}
-//       </Routes>
-//     </BrowserRouter>
-//   </div>
-//   , document.getElementById('root')
-// );
